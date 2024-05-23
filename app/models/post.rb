6e0d331ac56d7cc3d82cfc420340d1bd1999248e
recillah khamala <recillahk@gmail.com>
2024-05-23 14:54:51 +0300

    Configure Active Storage for handling image uploads

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many_attached :media_files
+
+  validate :media_files_presence
+
+  private
+
+  def media_files_presence
+    errors.add(:media_files, 'are required') unless media_files.attached?
+  end
 end
